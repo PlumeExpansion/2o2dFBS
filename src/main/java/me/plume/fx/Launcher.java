66b4f5021b291db7eb41e6abfac4de5bfe64c1cd
SparkingToaster <plume.expansion@gmail.com>
2024-06-08 16:19:48 -0400

    init commit

---
+++ src/main/java/me/plume/fx/Launcher.java
@@ -0,0 +1,14 @@
+package me.plume.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+	public void start(Stage window) {
+		window.setTitle("Maven Fx");
+		window.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
